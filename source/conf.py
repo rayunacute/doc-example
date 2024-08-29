622b982ef6906efb489115cff890274e7c1192d6
rayunacute <120047112+rayunacute@users.noreply.github.com>
2024-08-29 11:55:27 +0200

    first commit

---
+++ source/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = '> Separate source and build directories (y/n) [n]: <hit enter>'
+copyright = 'Project name: DIGGMINSKOLE'
+author = '> Project name: DIGGMINSKOLE'
+release = '> Author name(s): Azimil Gani Alam'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['myst_parser']
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = '> Project release []: 0.1'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
